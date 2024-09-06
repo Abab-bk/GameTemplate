09578893baf85565654edac33b21a4c8b5864d79
Abab-bk <55276676+Abab-bk@users.noreply.github.com>
2024-09-06 15:46:58 +0800

    更新luban、bug fix、优化表格样式

--- RenameProject.py
+++ RenameProject.py
@@ -2,14 +2,11 @@ import os
 
 def replace_string_in_file(file_name, search_string, replace_string):
     try:
-        # 读取文件内容
         with open(file_name, 'r', encoding='utf-8') as file:
             content = file.read()
         
-        # 替换字符串
         content = content.replace(search_string, replace_string)
         
-        # 写回文件
         with open(file_name, 'w', encoding='utf-8') as file:
             file.write(content)
         
@@ -29,12 +26,10 @@ def rename_file(old_name, new_name):
         print(f"Error when renaming: {e}")
 
 def main():
-    # 获取用户输入
     file_names = ["Src/MyNewGame.csproj", "Src/MyNewGame.sln", "Src/MyNewGame.sln.DotSettings.user"]
     project_name = input("input new project name: ")
     search_string = "MyNewGame"
     
-    # 确保文件在当前目录下
     for file_name in file_names:
         if os.path.isfile(file_name):
             replace_string_in_file(file_name, search_string, project_name)
@@ -43,8 +38,8 @@ def main():
             print(f"文件 '{file_name}' 不在当前目录下。")
     
     if os.path.isfile("Src/project.godot"):
-        replace_string_in_file(file_name, search_string, project_name)
-        replace_string_in_file(file_name, search_string, project_name)
+        replace_string_in_file("Src/project.godot", search_string, project_name)
+        replace_string_in_file("Src/project.godot", search_string, project_name)
 
     input("Press Enter to exit...")
 
