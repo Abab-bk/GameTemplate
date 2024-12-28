4a8acf27bf1acfc59041d740b5541ab91748e499
Abab-bk <55276676+Abab-bk@users.noreply.github.com>
2024-12-28 18:54:20 +0800

    feat: Add InitProject.py

--- InitProject.py
+++ InitProject.py
@@ -25,9 +25,19 @@ def rename_file(old_name, new_name):
     except Exception as e:
         print(f"Error when renaming: {e}")
 
+def delete_file(file_name):
+    try:
+        os.remove(file_name)
+        print(f"File '{file_name}' deleted.")
+    except FileNotFoundError:
+        print(f"File '{file_name}' not found.")
+    except Exception as e:
+        print(f"Error when deleting: {e}")
+
 def main():
     file_names = ["Src/MyNewGame.csproj", "Src/MyNewGame.sln", "Src/MyNewGame.sln.DotSettings.user"]
-    project_name = input("input new project name: ")
+    print("Step 1. Rename project")
+    project_name = input("Input your project name: ")
     search_string = "MyNewGame"
     
     for file_name in file_names:
@@ -35,11 +45,17 @@ def main():
             replace_string_in_file(file_name, search_string, project_name)
             rename_file(file_name, file_name.replace(search_string, project_name))
         else:
-            print(f"文件 '{file_name}' 不在当前目录下。")
+            print(f"File '{file_name}' not found.")
     
     if os.path.isfile("Src/project.godot"):
         replace_string_in_file("Src/project.godot", search_string, project_name)
         replace_string_in_file("Src/project.godot", search_string, project_name)
+
+    print("Step 2. Delete unnecessary files")
+    for file_name in ["CHANGELOG.md", "cz.toml"]:
+        delete_file(file_name)
+
+    print("Everything is done!")
 
     input("Press Enter to exit...")
 
